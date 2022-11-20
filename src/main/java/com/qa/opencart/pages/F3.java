08372a8df755523e624c29150d1242a53be03dd2
jayashreeasai26 <jayashreeasai26@gmail.com>
2022-11-20 18:56:24 +0530

    deleted unused files

--- src/main/java/com/qa/opencart/pages/F3.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class F3 {
-	
-	public static void main(String[]args){
-    
-    int m=10;
-	}
-}
--- src/main/java/com/qa/opencart/pages/F4.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class F4 {
-	
-	public static void main(String[]args){
-    
-    int k=10;
-	}
-}
